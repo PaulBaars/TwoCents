02cd0ace51dfd6143bac348c140d99d7bb7c26a7
PBaars <paul.baars@motion10.com>
2016-01-10 07:55:50 -0800

    Added Model and Controller

---
+++ Calculator/CalculatorService/Models/CalcInput.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace CalculatorService.Models
+{
+    public class CalcInput
+    {
+        [JsonProperty(PropertyName = "a")]
+        public int a;
+
+        [JsonProperty(PropertyName = "b")]
+        public int b;
+    }
+
+}
