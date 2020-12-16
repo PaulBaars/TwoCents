438d9fbc88281c83f508d9995080d025f7ee1816
Paul Baars <paul.baars@motion10.com>
2020-12-16 10:31:31 +0100

    Clean Repository

--- TwoCents.Azure.SQLScripts/3c. InsertProcessDefinitions.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO [dbo].[ProcessDefinitions]([ProcessId],[StepId],[Sequence],[Status],[Notification],[IsLastStep])
-VALUES (1,1,1,'Pending','None',1)
-
-INSERT INTO [dbo].[ProcessDefinitions]([ProcessId],[StepId],[Sequence],[Status],[Notification],[IsLastStep])
-VALUES (2,2,1,'Pending','None',1)
-
-GO
